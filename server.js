5cad6ce6955eff8ed9057b33fba2333b901bad77
dayamll <dayamllerenacastro@gmail.com>
2018-02-15 05:46:49 -0500

    Añadiendo estructura de archivos html

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = app.listen(3008, encender);
+const server = app.listen(3001, encender);
 
 function encender() {
   console.log('Servidor encendido');
