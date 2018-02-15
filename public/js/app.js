62ce79a2178f51962c523cfc8dbdedfb5318f350
dayamll <dayamllerenacastro@gmail.com>
2018-02-15 05:50:39 -0500

    Añadiendo variables

--- public/js/app.js
+++ public/js/app.js
@@ -4,4 +4,23 @@ $(document).ready(function() {
   let btnWeek = $('.weather');
   btnWeek.on('click', searchDay);
 
+  function getWeather(data) {
+    let weatherWeek = $('.icon-weather');
+    let week = $('#container-days');
+    let dayResponse = data.currently;
+    let weekResponse = data.daily.data;
+    let iconWeather = dayResponse.icon;
+    weatherWeek.attr('src', `assets/images/${dayResponse.icon}.png`);
+    let daysWeather = weekResponse.slice(0, 7);
+
+    daysWeather.forEach((element, index) => {
+      let model = `<div class = "row">
+      <div class="col-3"><img class="w100" src="../assets/images/${element.icon}.png"></div>
+      <div class="col-3"><p>${days[index]}</p></div>
+      <div class="col-3"><p>${element.temperatureMin}°</p></div>
+      <div class="col-3"><p>${element.temperatureMax}°</p></div>
+      </div>`;
+      week.append(model);
+    });
+  }
 });
