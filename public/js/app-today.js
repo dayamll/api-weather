7528120e2a60d97a0cb9c5d16d003cc360bbbb4e
margb13 <marg.b13@hotmail.com>
2018-02-15 16:59:32 -0500

    Añadiendo funcionalidad para la temperaatura del día y organizando los archivos

--- public/js/app-today.js
+++ public/js/app-today.js
@@ -5,6 +5,12 @@ $(document).ready(function () {
     window.location.href = 'view/today.html';
   });
 
+  // Abrir vista week.html
+  let btnOpenWeek = $('.open-week');
+  btnOpenWeek.click(function () {
+    window.location.href = 'week.html';
+  });
+
   // Función para la geolocalización
   searchPosition();
   function searchPosition() {
@@ -27,5 +33,45 @@ $(document).ready(function () {
     } else {
       console.log('Su navegador no soporta Geolocalización');
     }
+  }
+
+  // Objeto de dias
+  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
+  // Función para la geolocalización
+  function getWeather(data) {
+    let imgWeather = $('.icon-weather');
+    let contentWeek = $('#content-week');
+    let temperature = $('.temperature');
+    let wind = $('#wind');
+    let humidity = $('#humidity');
+    let uvIndex = $('#uv-index');
+    let pressure = $('#pressure');
+    let summary = $('#summary');
+    let responseToday = data.currently;
+    let responseWeek = data.daily.data;
+    let icon = responseToday.icon;
+    console.log(responseToday);
+    // Reemplazamos por los valores de la data para la vista today
+    temperature.text(responseToday.temperature + '°');
+    wind.text(responseToday.windSpeed + 'm/s');
+    humidity.text(responseToday.humidity + '%');
+    uvIndex.text(responseToday.uvIndex);
+    pressure.text(responseToday.pressure + 'hPa');
+    summary.text(responseToday.summary);
+    // Iconos del clima
+    imgWeather.attr('src', `../assets/images/${responseToday.icon}.png`);
+    // result week
+    let daysWeather = responseWeek.slice(0, 7);
+
+    daysWeather.forEach((element, index) => {
+      let modelTemplate = `<div class = "row">
+      <div class="col-2"><img class="w100" src="../assets/images/${element.icon}.png"></div>
+      <div class="col-5"><p class="font-1">${days[index]}</p></div>
+      <div class="col-2"><p class="font-1">${element.temperatureMin}°</p></div>
+      <div class="col-2"><p class="font-1">${element.temperatureMax}°</p></div>
+      </div>`;
+      contentWeek.append(modelTemplate);
+    });
   }
 });
--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,29 @@
-$(document).ready(function () {
+$(document).ready(function() {
   searchWeather();
-  const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
   let btnWeek = $('.weather');
   btnWeek.on('click', searchDay);
 
   function getWeather(data) {
-    // Para days.html
     let weatherWeek = $('.icon-weather');
-    let wind = $('#wind');
-    let humidity = $('#humidity');
-    let uvIndex = $('#uv-index');
-    let pressure = $('#pressure');
-    let summary = $('#summary');
-    let dayResponse = data.currently;
-    let icon = dayResponse.icon;
-    console.log(dayResponse);
-    temperature.text(dayResponse.temperature + '°');
-    wind.text(dayResponse.windSpeed + 'm/s');
-    humidity.text(dayResponse.humidity + '%');
-    uvIndex.text(dayResponse.uvIndex);
-    pressure.text(dayResponse.pressure + 'hPa');
-    summary.text(dayResponse.summary);
-
-
-    let responseWeek = data.daily.data;
-
     let week = $('#container-days');
-
+    let dayResponse = data.currently;
     let weekResponse = data.daily.data;
     let iconWeather = dayResponse.icon;
-
     weatherWeek.attr('src', `assets/images/${dayResponse.icon}.png`);
     let daysWeather = weekResponse.slice(0, 7);
 
     daysWeather.forEach((element, index) => {
       let model = `<div class = "row">
       <div class="col-3"><img class="w100" src="../assets/images/${element.icon}.png"></div>
-      <div class="col-3"><p>${days[index]}</p></div>
-      <div class="col-3"><p>${element.temperatureMin}°</p></div>
-      <div class="col-3"><p>${element.temperatureMax}°</p></div>
+      <div class="col-3 space-title"><p>${days[index]}</p></div>
+      <div class="col-3 space-title"><p>${element.temperatureMin}°</p></div>
+      <div class="col-3 space-title"><p>${element.temperatureMax}°</p></div>
       </div>`;
       week.append(model);
     });
   }
-  // Geolocation
+
   function searchWeather() {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((position) => {
@@ -63,9 +43,5 @@ $(document).ready(function () {
     } else {
       console.log('Existe un error en el sistema');
     }
-  }
-
-  function searchDay() {
-    window.location.href = 'view/days.html';
   }
 });
